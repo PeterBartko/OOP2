8d00806e864797c6eb456b9eb99f15eead8c0b0b
Peter Bartko <xbartkop@stuba.sk>
2021-02-18 14:33:09 +0100

    1-10

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,5 +1,10 @@
 package sk.stuba.fei.uim.oop;
 
 public class App {
-
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++)
+            System.out.print(i + " ");
+    }
 }
+
+
